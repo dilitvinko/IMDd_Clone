7760d4b3ca2257740c02ae35945228d2fa8de6a7
dlitvinko <dlitvinko@objectstyle.com>
2024-04-15 13:54:54 +0300

    initial commit

---
+++ src/main/java/com/my/project/imdd_clone/ImDdCloneApplication.java
@@ -0,0 +1,13 @@
+package com.my.project.imdd_clone;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ImDdCloneApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ImDdCloneApplication.class, args);
+    }
+
+}
